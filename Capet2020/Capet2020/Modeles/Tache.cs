b4ffca7106c9c3da86f70458118b6b5a9ecf4700
pierro35000 <pierrebarbe0@gmail.com>
2020-03-02 16:20:29 +0100

    coucou éline

--- Capet2020/Capet2020/Modeles/Tache.cs
+++ Capet2020/Capet2020/Modeles/Tache.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,41 @@ namespace Capet2020.Modeles
 {
     class Tache
     {
+        #region Attribut 
+        public static ArrayList CollClassTache = new ArrayList();
+        private ArrayList lesCollaborateurs;
+
+        private int _idTache;
+        private string _intituleTache;
+        private DateTime _dateDebutPrevue;
+        private DateTime _dateDebitRelle;
+        private int _dureePrevue;
+        private int _dureeRelle;
+        private string _etat;
+        #endregion
+
+        #region Constructeur
+        public Tache(int idTache, string intituleTache, DateTime dateDebutPrevue, DateTime dateDebitRelle, int dureePrevue, int dureeRelle, string etat)
+        {
+            IdTache = idTache;
+            IntituleTache = intituleTache;
+            DateDebutPrevue = dateDebutPrevue;
+            DateDebitRelle = dateDebitRelle;
+            DureePrevue = dureePrevue;
+            DureeRelle = dureeRelle;
+            Etat = etat;
+            this.lesCollaborateurs = new ArrayList();
+        }
+        #endregion
+
+        #region Getter / Setter 
+        public int IdTache { get => _idTache; set => _idTache = value; }
+        public string IntituleTache { get => _intituleTache; set => _intituleTache = value; }
+        public DateTime DateDebutPrevue { get => _dateDebutPrevue; set => _dateDebutPrevue = value; }
+        public DateTime DateDebitRelle { get => _dateDebitRelle; set => _dateDebitRelle = value; }
+        public int DureePrevue { get => _dureePrevue; set => _dureePrevue = value; }
+        public int DureeRelle { get => _dureeRelle; set => _dureeRelle = value; }
+        public string Etat { get => _etat; set => _etat = value; }
+        #endregion
     }
 }
