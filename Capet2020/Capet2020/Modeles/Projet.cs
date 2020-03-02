c4e3593987af8c8eaeb17a5159be669218d1a78c
Eline Tvnd <eline.thivend@gmail.com>
2020-03-02 16:05:03 +0100

    classes

--- Capet2020/Capet2020/Modeles/Projet.cs
+++ Capet2020/Capet2020/Modeles/Projet.cs
@@ -9,16 +9,38 @@ namespace Capet2020.Modeles
 {
     class Projet
     {
+        #region attributs
         public static ArrayList CollClassProjet = new ArrayList();
-        private int idProjet;
-        private string intituleProjet;
-        private DateTime dateDebutPrevue;
-        private DateTime dateDebutReelle;
-        private DateTime dateFinPrevue;
-        private DateTime dateFinReelle;
-        private string etat;
-        //private Responsable leResponsable;
+
+        private int _idProjet;
+        private string _intituleProjet;
+        private DateTime _dateDebutPrevue;
+        private DateTime _dateDebutReelle;
+        private DateTime _dateFinPrevue;
+        private DateTime _dateFinReelle;
+        private string _etat;
+
+        private Responsable leResponsable;
         private ArrayList lesLots;
-        //private Dictionary<DateTime, Tache> lesTachesRestantARealiser;
+        private Dictionary<DateTime, Tache> lesTachesRestantARealiser;
+        #endregion
+
+        #region Constructeurs
+        public Projet(int idProjet, string intituleProjet, DateTime dateDebutPrevue, DateTime dateDebutReelle, DateTime dateFinPrevue, DateTime dateFinReelle, string etat, Responsable leResponsable)
+        {
+            _idProjet = idProjet;
+            _intituleProjet = intituleProjet;
+            _dateDebutPrevue = dateDebutPrevue;
+            _dateDebutReelle = dateDebutReelle;
+            _dateFinPrevue = dateFinPrevue;
+            _dateFinReelle = dateFinReelle;
+            _etat = etat;
+            lesLots = new ArrayList();
+            leResponsable = leResponsable;
+
+            CollClassProjet.Add(this);
+        }
+        #endregion
+
     }
 }
