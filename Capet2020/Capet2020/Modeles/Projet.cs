2da63a7433d7158792e5ff57564cf67e64a77c57
Eline Tvnd <eline.thivend@gmail.com>
2020-03-02 15:56:08 +0100

    projet

---
+++ Capet2020/Capet2020/Modeles/Projet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capet2020.Modeles
+{
+    class Projet
+    {
+        public static ArrayList CollClassProjet = new ArrayList();
+        private int idProjet;
+        private string intituleProjet;
+        private DateTime dateDebutPrevue;
+        private DateTime dateDebutReelle;
+        private DateTime dateFinPrevue;
+        private DateTime dateFinReelle;
+        private string etat;
+        //private Responsable leResponsable;
+        private ArrayList lesLots;
+        //private Dictionary<DateTime, Tache> lesTachesRestantARealiser;
+    }
+}
