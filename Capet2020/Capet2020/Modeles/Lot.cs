2f9352d810f3adda8b821dcb9cf704a9f6422c92
Eline Tvnd <eline.thivend@gmail.com>
2020-03-09 09:45:16 +0100

    v;tckuc

--- Capet2020/Capet2020/Modeles/Lot.cs
+++ Capet2020/Capet2020/Modeles/Lot.cs
@@ -104,7 +104,7 @@ namespace Capet2020.Modeles
             {
                 this.LesTaches.Add(LeProjet.LesTachesRestantARealiser.First().Key);
                 LeProjet.LesTachesRestantARealiser.First().Key.Etat = "a affecter";
-                LeProjet.LesTachesRestantARealiser.Remove(LeProjet.LesTachesRestantARealiser.First().Key);
+                LeProjet.LesTachesRestantARealiser.Remove(LeProjet.LesTachesRestantARealiser.ElementAt(0).Key);
             }
         }
         #endregion
